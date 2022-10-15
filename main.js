4aa5e528c9ea0ef42a5177219f0c827e5acb9853
revanthreddy123 <ar.revanthreddy@gmail.com>
2022-10-16 03:55:01 +0530

    Update main.js

--- main.js
+++ main.js
@@ -3,6 +3,8 @@ const imageContainer = document.querySelector(".image-container");
 
 const viewer = new PANOLENS.Viewer({
   container: imageContainer,
+  autoRotate: true,
+  autoRotateSpeed: 0.3,
 });
 
 viewer.add(panoramaImage);
